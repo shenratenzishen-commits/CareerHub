a16f8d795e0aff35d1a87ad31c4ac3072c40897a
christianyee2003 <christianyee2003@gmail.com>
2026-06-01 17:07:49 +0800

    Remove duplicate LARAVEL_START from api/index.php

--- api/index.php
+++ api/index.php
@@ -1,9 +1,5 @@
 <?php
 
-if (!defined('LARAVEL_START')) {
-    define('LARAVEL_START', microtime(true));
-}
-
 $_SERVER['SCRIPT_FILENAME'] = __DIR__ . '/../public/index.php';
 $_SERVER['DOCUMENT_ROOT']   = __DIR__ . '/../public';
 
