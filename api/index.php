9ff87db16cfd506a71ece2b006f699c04dd70045
christianyee2003 <christianyee2003@gmail.com>
2026-06-01 17:06:24 +0800

    Fix LARAVEL_START already defined warning

--- api/index.php
+++ api/index.php
@@ -1,6 +1,8 @@
 <?php
 
-define('LARAVEL_START', microtime(true));
+if (!defined('LARAVEL_START')) {
+    define('LARAVEL_START', microtime(true));
+}
 
 $_SERVER['SCRIPT_FILENAME'] = __DIR__ . '/../public/index.php';
 $_SERVER['DOCUMENT_ROOT']   = __DIR__ . '/../public';
